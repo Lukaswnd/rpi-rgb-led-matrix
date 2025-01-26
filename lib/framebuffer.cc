0584ec0b3a9a2a1edc94fa29555ab4ddd69c62cb
Lukaswnd <Lukas+git@windeln.nrw>
2025-01-27 00:16:00 +0100

    fix SetPixelBytes double b and remove unneccessary calculation

--- lib/framebuffer.cc
+++ lib/framebuffer.cc
@@ -719,10 +719,10 @@ void Framebuffer::SetPixels32(int x, int y, int width, int height, uint32_t **co
 void Framebuffer::SetPixelBytes(int x, int y, int width, int height, uint8_t *bytes) {
   for (int iy = 0; iy < height; ++iy) {
     for (int ix = 0; ix < width; ++ix) {
-      uint8_t* b =  bytes + (((iy*width) + ix)*3);
-      uint8_t r = (b[0]) & 0xFF;
-      uint8_t g = (b[1] >> 8) & 0xFF;
-      uint8_t b = (b[2] >> 16) & 0xFF;
+      uint8_t* byte =  bytes + (((iy*width) + ix)*3);
+      uint8_t r = byte[0];
+      uint8_t g = byte[1];
+      uint8_t b = byte[2];
       SetPixel(x + ix, y + iy, r, g, b);
     }
   }
